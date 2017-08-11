02aeba7c387b0c007433d94ca5371f11a5f8c5d1
DESKTOP-TKBMKTJ\Admin <852238065@qq.com>
2017-08-11 21:35:03 +0800

    添加

---
+++ FormsCTF/frmControl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsCTF
+{
+    public partial class frmControl : Form
+    {
+        public frmControl()
+        {
+            InitializeComponent();
+        }
+        public delegate void ObjControl
+        private void frmControl_Load(object sender, EventArgs e)
+        {
+            new Thread(() => {
+                for (int i = 0; i < 1000; i++)
+                {
+                    label1.Text = i.ToString();
+                }
+            }).Start();
+        }
+    }
+}
